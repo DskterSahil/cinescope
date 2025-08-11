f3100669689aa8c5c1a8e3b729153f513e243c5d
Dskter <dskter.sahil@gmail.com>
2025-08-11 21:34:13 +0530

    added db on render

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,16 @@
 import pkg from "pg";
 
-const {Pool} = pkg;
+const { Pool } = pkg;
 
 const pool = new Pool({
-    user: 'dskter',
-    host: 'localhost',
-    database: 'cinescope',
-    password: 'dskter',
-    port: 5432,
-})
+  user: 'dskter',
+  host: 'dpg-d2d0tm2dbo4c73c7kg2g-a.oregon-postgres.render.com',
+  database: 'cinescopedb',
+  password: '4ZMLQYtPEnESYbKaRoqce2zjmgaU98fG',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false  // important for Render SSL connection
+  }
+});
 
-export default pool
+export default pool;
